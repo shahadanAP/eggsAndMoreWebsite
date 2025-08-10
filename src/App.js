2b9e7e38148f877d1d798d0ef8b14faef87c07bf
shahadanAP <shahadanap@gmail.com>
2025-08-10 17:39:05 +0530

    Update all files no erros for ratings page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import HomePage from './HomePage';
 import MenuPage from './MenuPage';
-import AboutPage from './AboutPage';
 import ContactPage from './ContactPage';
+import AboutPage from './AboutPage';
+import RatingPage from './RatingPage';          // ← Restored
+import NotFoundPage from './NotFoundPage';     // ← Restored
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        {/* Index route (home page) */}
         <Route index element={<HomePage />} />
+        
+        {/* Main pages */}
         <Route path="menu" element={<MenuPage />} />
         <Route path="about" element={<AboutPage />} />
         <Route path="contact" element={<ContactPage />} />
+        
+        {/* Nested rating route (restored!) */}
+        <Route 
+          path="menu/rate/:menuType/:category/:itemName" 
+          element={<RatingPage />} 
+        />
+        
+        {/* Catch-all 404 route (restored!) */}
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
--- src/Layout.js
+++ src/Layout.js
@@ -1,13 +1,14 @@
+// Layout.js
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
-import './App.css';
+import './App.css'; // Make sure your styles are imported
 
 export default function Layout() {
   return (
     <div className="app">
       <Navbar />
       <main className="main-content">
-        <Outlet />
+        <Outlet /> {/* This renders the current page */}
       </main>
     </div>
   );
